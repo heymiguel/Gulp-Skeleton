492aeab6cd08558e8d283f56bc75723f23aeba93
LucasAlt <lucasalbert67@gmail.com>
2017-10-28 15:43:54 +0200

    Include Jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,63 @@
-'use strict';
+'use strict'
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    cssmin      = require('gulp-cssmin'),
-    rename      = require('gulp-rename'),
-    prefix      = require('gulp-autoprefixer'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    imagemin    = require('gulp-imagemin'),
-    browserSync = require('browser-sync').create();
+var gulp = require('gulp')
+var jade = require('gulp-jade')
+var sass = require('gulp-sass')
+var cssmin = require('gulp-cssmin')
+var rename = require('gulp-rename')
+var prefix = require('gulp-autoprefixer')
+var uglify = require('gulp-uglify')
+var concat = require('gulp-concat')
+var imagemin = require('gulp-imagemin')
+var browserSync = require('browser-sync').create()
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js'], function() {
-
-    browserSync.init({
-        server: './'
-    });
-
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-});
+// Compile Jade
+gulp.task('jade', function () {
+  gulp.src('src/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream())
+})
 
 // Configure CSS tasks.
 gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
+  gulp.src('src/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
     .pipe(concat('styles.css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 // Configure JS.
-gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
+gulp.task('js', function () {
+  gulp.src('src/js/**/*.js')
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 // Configure image stuff.
 gulp.task('images', function () {
-  return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+  gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
-});
+    .pipe(gulp.dest('dist/img'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-});
+gulp.task('default', ['jade', 'sass', 'js', 'images'], function () {
+  browserSync.init({
+    server: './dist'
+  })
 
-gulp.task('default', ['sass', 'js', 'images', 'serve']);
+  gulp.watch('src/*.jade', ['jade'])
+  gulp.watch('src/scss/**/*.scss', ['sass'])
+  gulp.watch('src/js/**/*.js', ['js'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
