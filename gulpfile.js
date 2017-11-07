0c87a3563acb2168ba94a25bb0b30f5a82c4d59b
heymiguel <miguelcesarbautista@gmail.com>
2017-11-06 21:17:13 -0500

    Suck it google

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var cssmin = require('gulp-cssmin')
-var rename = require('gulp-rename')
-var prefix = require('gulp-autoprefixer')
-var uglify = require('gulp-uglify')
-var concat = require('gulp-concat')
-var imagemin = require('gulp-imagemin')
-var browserSync = require('browser-sync').create()
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const babel = require('gulp-babel')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
+const prefix = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const imagemin = require('gulp-imagemin')
+const browserSync = require('browser-sync').create()
 
 // Compile html
 gulp.task('html', function () {
@@ -32,9 +33,12 @@ gulp.task('sass', function () {
 // Configure JS.
 gulp.task('js', function () {
   return gulp.src('src/js/**/*.js')
-    .pipe(uglify())
     .pipe(concat('app.js'))
+    .pipe(babel({
+      presets: ['env']
+    }))
     .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream())
 })
@@ -54,6 +58,6 @@ gulp.task('default', ['html', 'sass', 'js', 'images'], function () {
 
   gulp.watch('src/*.html', ['html'])
   gulp.watch('src/scss/**/*.scss', ['sass'])
-  gulp.watch('src/js/**/*.js', ['js'])
+  gulp.watch('src/js/**/*.js', ['js']).on('change', browserSync.reload)
   gulp.watch('./dist/*.html').on('change', browserSync.reload)
 })
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,26 @@
 // Place JS here.
+function eventHandlers () {
+  const howMany = document.getElementsByName('numguests')
+  const howManyField = document.getElementById('how-many')
+  howMany.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+      howManyField.value = e.target.value
+    })
+  })
+
+  const where = document.getElementsByName('location')
+  const whereField = document.getElementById('where')
+  where.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+      whereField.value = e.target.value
+    })
+  })
+};
+
+if (document.readyState === 'complete' || document.readyState !== 'loading') {
+  eventHandlers()
+} else {
+  document.addEventListener('DOMContentLoaded', () =>{
+    eventHandlers()
+  })
+}
